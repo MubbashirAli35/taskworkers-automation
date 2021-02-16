e42f44d8f8ee8a16b84521fd10f54162e61eb399
Mubbashir Ali <mubbashirali35@gmail.com>
2021-02-16 22:50:46 +0500

    Cookies fixed for gctw25

--- gctw25.py
+++ gctw25.py
@@ -12,9 +12,9 @@ notebooks_config = pd.read_csv(sys.argv[1])
 
 # Adds configuration to the chromedriver
 options = Options()
-options.add_argument('--user-data-dir=C:/Users/mubba/AppData/Local/Google/Chrome/User Data')
-options.add_argument('--profile-directory=Profile 28')
-# options.add_argument('headless')    # Configures to start chrome in headless mode
+# options.add_argument('--user-data-dir=C:/Users/mubba/AppData/Local/Google/Chrome/User Data')
+# options.add_argument('--profile-directory=Profile 28')
+options.add_argument('headless')    # Configures to start chrome in headless mode
 options.add_argument('--start-maximized')   # Configures to start it with maximum window size
 
 # Adds a specific User Agent
@@ -34,19 +34,19 @@ notebooks = ['https://colab.research.google.com/drive/11-mJwtKRyuV8ShalFp8p8qvP2
 
 i = 0
 
-with Chrome(executable_path='./chromedriver_win32/chromedriver.exe', options=options) as driver:
+with Chrome(executable_path='./chromedriver', options=options) as driver:
     # driver.get('https://accounts.google.com/o/oauth2/auth/identifier?client_id=717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com&scope=profile%20email&redirect_uri=https%3A%2F%2Fstackauth.com%2Fauth%2Foauth2%2Fgoogle&state=%7B%22sid%22%3A1%2C%22st%22%3A%2259%3A3%3Abbc%2C16%3Aaa5bbfcd04987df9%2C10%3A1609186223%2C16%3Ae0f9e400ff758b8a%2Cc93369a30a25c98422fa6bbedbcd77dca39193057a5efe32ea8b0bb0c24a2d50%22%2C%22cdl%22%3Anull%2C%22cid%22%3A%22717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com%22%2C%22k%22%3A%22Google%22%2C%22ses%22%3A%2200c68903dfa345e59f106379ca35fdb8%22%7D&response_type=code&flowName=GeneralOAuthFlow')
 
     driver.get(notebooks[0])
 
-    # for cookie in pickle.load(open('./cookies/cookies_gctw25.pkl', 'rb')):
-    #     if 'sameSite' in cookie:
-    #         if cookie['sameSite'] == 'None':
-    #             cookie['sameSite'] = 'Strict'
-    #     driver.add_cookie(cookie)
+    for cookie in pickle.load(open('./cookies/cookies_gctw25.pkl', 'rb')):
+        if 'sameSite' in cookie:
+            if cookie['sameSite'] == 'None':
+                cookie['sameSite'] = 'Strict'
+        driver.add_cookie(cookie)
 
-    time.sleep(5)
-    pickle.dump(driver.get_cookies(), open('./cookies/cookies_gctw25.pkl', 'wb'), protocol=2)
+    # time.sleep(5)
+    # pickle.dump(driver.get_cookies(), open('./cookies/cookies_gctw25.pkl', 'wb'), protocol=2)
 
     for i in range(9):
         if notebooks_config.at[i + 216, 'Status'] == 'Yes':
