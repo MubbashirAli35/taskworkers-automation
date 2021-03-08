769340ab4df24640cbd5e81eced31697a1d6cc17
Mubbashir Ali <mubbashirali35@gmail.com>
2021-03-09 02:19:31 +0500

    Alive Status column calculated and added to DataFrame

--- run_workers.py
+++ run_workers.py
@@ -5,11 +5,10 @@ import os
 import re
 
 def run_notebook(notebook_name):
-    if re.match('[0-9]', notebook[5:6]) == None:
+    if re.match('[0-9]', notebook_name[5:6]) == None:
         print('python ' + notebook_name[0:5] + '/' + notebook_name + '.py' + ' ' + notebook_name)
     else:
         print('python ' + notebook_name[0:6] + '/' + notebook_name + '.py' + ' ' + notebook_name)
-
 
 conn = psycopg2.connect(dbname='greencanvas',
                         user='gcp_read_only',
@@ -46,8 +45,13 @@ notebooks.dropna(inplace=True)
 backtests_notebooks = notebooks[~notebooks['Notebook'].str.contains('GPU')]
 training_notebooks = notebooks[notebooks['Notebook'].str.contains('GPU')]
 
+backtests_notebooks['Alive Status'] = dt.datetime.now() - backtests_notebooks['Max Last Beat Time'] < dt.timedelta(minutes=20)
+training_notebooks['Alive Status'] = dt.datetime.now() - training_notebooks['Max Last Beat Time'] < dt.timedelta(minutes=20)
+
 backtests_notebooks_sorted_on_last_beat_time = backtests_notebooks.sort_values(by='Max Last Beat Time', ignore_index=True)
 training_notebooks_sorted_on_last_beat_time = training_notebooks.sort_values(by='Max Last Beat Time', ignore_index=True)
+
+# print(backtests_notebooks)
 
 # Only keep notebooks that ran not more than two hours ago
 backtests_notebooks_sorted_on_last_beat_time = backtests_notebooks_sorted_on_last_beat_time.loc[
@@ -61,29 +65,10 @@ training_notebooks_sorted_on_last_beat_time = training_notebooks_sorted_on_last_
 backtest_workers = backtests_notebooks_sorted_on_last_beat_time['Notebook']
 training_workers = training_notebooks_sorted_on_last_beat_time['Notebook']
 
-# for notebook in backtest_workers:
-#     run_notebook(notebook)
+
 
 num_of_pending_backtests = tasks_queue.loc[(tasks_queue['Status'] == 'New') & (tasks_queue['Task Type'] == 'BackTestTask')]['Number']
 num_of_pending_training_tasks = tasks_queue.loc[(tasks_queue['Status'] == 'New') & (tasks_queue['Task Type'] == 'Training')]['Number']
 
 num_of_backtests_running = tasks_queue.loc[(tasks_queue['Status'] == 'Running') & (tasks_queue['Task Type'] == 'BackTestTask')]['Number']
 num_of_training_tasks_running = tasks_queue.loc[(tasks_queue['Status'] == 'Running') & (tasks_queue['Task Type'] == 'Training')]['Number']
-
-# if num_of_pending_backtests.count() > 0:
-#     if num_of_pending_backtests.iloc[0] > 0:
-#         print('Number of Backtests pending: ' + str(num_of_pending_backtests.iloc[0]))
-#
-#         if num_of_backtests_running.count() > 0:
-#             if num_of_backtests_running.iloc[0] < 75:
-#                 for i in range(0, 75 - num_of_backtests_running.iloc[0]):
-#                     print('python ' + backtests_notebooks_sorted_on_last_beat_time.iloc[i]['Notebook'])
-#             else:
-#                 print('Max allowed backtest notebooks running')
-#         else:
-#             if backtests_notebooks_sorted_on_last_beat_time['Notebook'].count() >= 75:
-#                 for i in range(75):
-#                     print('python ' + backtests_notebooks_sorted_on_last_beat_time.iloc[i]['Notebook'])
-#             else:
-#                 for i in range(backtests_notebooks_sorted_on_last_beat_time['Notebook'].count()):
-#                     print('python ' + backtests_notebooks_sorted_on_last_beat_time.iloc[i]['Notebook'])
