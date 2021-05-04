c7e10a7b5d7c2d61b017f10f561349288b96da7c
Mubbashir Ali <mubbashirali35@gmail.com>
2021-05-05 01:49:26 +0500

    Update: Kmeans tasks support added

--- run_workers.py
+++ run_workers.py
@@ -70,9 +70,12 @@ training_notebooks_to_interact = training_notebooks_to_interact['Notebook']
 
 num_of_pending_backtests = tasks_queue.loc[(tasks_queue['Status'] == 'New') & (tasks_queue['Task Type'] == 'BackTestTask')]['Number']
 num_of_pending_training_tasks = tasks_queue.loc[(tasks_queue['Status'] == 'New') & (tasks_queue['Task Type'] == 'TrainingTask')]['Number']
+num_of_pending_kmeans_tasks = tasks_queue.loc[(tasks_queue['Status'] == 'New') & (tasks_queue['Task Type'] == 'KmeansTask')]['Number']
 
 num_of_backtests_running = tasks_queue.loc[(tasks_queue['Status'] == 'Running') & (tasks_queue['Task Type'] == 'BackTestTask')]['Number']
 num_of_training_tasks_running = tasks_queue.loc[(tasks_queue['Status'] == 'Running') & (tasks_queue['Task Type'] == 'TrainingTask')]['Number']
+num_of_kmeans_tasks_running = tasks_queue.loc[(tasks_queue['Status'] == 'Running') & (tasks_queue['Task Type'] == 'KmeansTask')]['Number']
+
 
 if __name__ == '__main__':
     print(dt.datetime.now())
@@ -394,6 +397,176 @@ if __name__ == '__main__':
         else:
             print('No training tasks pending')
 
+        if num_of_pending_kmeans_tasks.count() > 0 and num_of_pending_kmeans_tasks.iloc[0] > 0 \
+                and (sys.argv[2] == 'train' or sys.argv[2] == 'both'):
+            notebook_1_ret_val = Queue()
+            notebook_2_ret_val = Queue()
+            notebook_3_ret_val = Queue()
+            notebook_4_ret_val = Queue()
+            notebook_5_ret_val = Queue()
+            notebooks_index = 0
+
+            print('Pending Kmeans Tasks: ', num_of_pending_kmeans_tasks.iloc[0])
+
+            if num_of_kmeans_tasks_running.count() > 0:
+                i = num_of_kmeans_tasks_running.iloc[0]
+                print('Num of Kmeans Tasks running: ', num_of_kmeans_tasks_running.iloc[0])
+
+                if num_of_kmeans_tasks_running.iloc[0] < num_of_pending_kmeans_tasks.iloc[0]:
+                    if sys.argv[1].lower() == 'run':
+                        while i < num_of_pending_kmeans_tasks:
+                            if i < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                                notebook_1 = Process(target=run_notebook,
+                                                     args=(backtests_notebooks_to_run[notebooks_index],
+                                                           notebook_1_ret_val,))
+                                notebook_1.start()
+                            if i + 1 < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                                notebook_2 = Process(target=run_notebook,
+                                                     args=(backtests_notebooks_to_run[notebooks_index + 1],
+                                                           notebook_2_ret_val,))
+                                notebook_2.start()
+                            if i + 2 < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                                notebook_3 = Process(target=run_notebook,
+                                                     args=(backtests_notebooks_to_run[notebooks_index + 2],
+                                                           notebook_3_ret_val,))
+                                notebook_3.start()
+                            if i + 3 < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                                notebook_4 = Process(target=run_notebook,
+                                                     args=(backtests_notebooks_to_run[notebooks_index + 3],
+                                                           notebook_4_ret_val,))
+                                notebook_4.start()
+                            if i + 4 < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                                notebook_5 = Process(target=run_notebook,
+                                                     args=(backtests_notebooks_to_run[notebooks_index + 4],
+                                                           notebook_5_ret_val,))
+                                notebook_5.start()
+
+                            if i < num_of_pending_kmeans_tasks.iloc[0]:
+                                if notebook_1_ret_val.get() == 1:
+                                    if i - 1 < num_of_kmeans_tasks_running.iloc[0]:
+                                        i = num_of_kmeans_tasks_running.iloc[0]
+                                    else:
+                                        i -= 1
+
+                                notebook_1.join()
+                            if i + 1 < num_of_pending_kmeans_tasks.iloc[0]:
+                                if notebook_2_ret_val.get() == 1:
+                                    if i - 1 < num_of_kmeans_tasks_running.iloc[0]:
+                                        i = num_of_kmeans_tasks_running.iloc[0]
+                                    else:
+                                        i -= 1
+
+                                notebook_2.join()
+                            if i + 2 < num_of_pending_kmeans_tasks.iloc[0]:
+                                if notebook_3_ret_val.get() == 1:
+                                    if i - 1 < num_of_kmeans_tasks_running.iloc[0]:
+                                        i = num_of_kmeans_tasks_running.iloc[0]
+                                    else:
+                                        i -= 1
+
+                                notebook_3.join()
+                            if i + 3 < num_of_pending_kmeans_tasks.iloc[0]:
+                                if notebook_4_ret_val.get() == 1:
+                                    if i - 1 < num_of_kmeans_tasks_running.iloc[0]:
+                                        i = num_of_kmeans_tasks_running.iloc[0]
+                                    else:
+                                        i -= 1
+
+                                notebook_4.join()
+                            if i + 4 < num_of_pending_kmeans_tasks.iloc[0]:
+                                if notebook_5_ret_val.get() == 1:
+                                    if i - 1 < num_of_kmeans_tasks_running.iloc[0]:
+                                        i = num_of_kmeans_tasks_running.iloc[0]
+                                    else:
+                                        i -= 1
+
+                                notebook_5.join()
+
+                            notebooks_index += 5
+                            i += 5
+            else:
+                if sys.argv[1].lower() == 'run':
+                    notebook_1_ret_val = Queue()
+                    notebook_2_ret_val = Queue()
+                    notebook_3_ret_val = Queue()
+                    notebook_4_ret_val = Queue()
+                    notebook_5_ret_val = Queue()
+                    notebooks_index = 0
+                    i = 0
+
+                    while i < num_of_pending_kmeans_tasks.iloc[0]:
+                        if i < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                            notebook_1 = Process(target=run_notebook,
+                                                 args=(backtests_notebooks_to_run[notebooks_index],
+                                                       notebook_1_ret_val,))
+                            notebook_1.start()
+                        if i + 1 < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                            notebook_2 = Process(target=run_notebook,
+                                                 args=(backtests_notebooks_to_run[notebooks_index + 1],
+                                                       notebook_2_ret_val,))
+                            notebook_2.start()
+                        if i + 2 < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                            notebook_3 = Process(target=run_notebook,
+                                                 args=(backtests_notebooks_to_run[notebooks_index + 2],
+                                                       notebook_3_ret_val,))
+                            notebook_3.start()
+                        if i + 3 < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                            notebook_4 = Process(target=run_notebook,
+                                                 args=(backtests_notebooks_to_run[notebooks_index + 3],
+                                                       notebook_4_ret_val,))
+                            notebook_4.start()
+                        if i + 4 < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                            notebook_5 = Process(target=run_notebook,
+                                                 args=(backtests_notebooks_to_run[notebooks_index + 4],
+                                                       notebook_5_ret_val,))
+                            notebook_5.start()
+
+                        if i < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                            if notebook_1_ret_val.get() == 1:
+                                if i - 1 < 0:
+                                    i = 0
+                                else:
+                                    i -= 5
+
+                            notebook_1.join()
+                        if i + 1 < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                            if notebook_2_ret_val.get() == 1:
+                                if i - 1 < 0:
+                                    i = 0
+                                else:
+                                    i -= 5
+
+                            notebook_2.join()
+                        if i + 2 < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                            if notebook_3_ret_val.get() == 1:
+                                if i - 1 < 0:
+                                    i = 0
+                                else:
+                                    i -= 5
+
+                            notebook_3.join()
+                        if i + 3 < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                            if notebook_4_ret_val.get() == 1:
+                                if i - 1 < 0:
+                                    i = 0
+                                else:
+                                    i -= 5
+
+                            notebook_4.join()
+                        if i + 4 < num_of_pending_kmeans_tasks.iloc[0] or i < 5:
+                            if notebook_5_ret_val.get() == 1:
+                                if i - 1 < 0:
+                                    i = 0
+                                else:
+                                    i -= 5
+
+                            notebook_5.join()
+
+                        notebooks_index += 5
+                        i += 5
+        else:
+            print('No Kmeans tasks pending')
+
         if num_of_pending_backtests.count() > 0 and num_of_pending_backtests.iloc[0] > 0 \
                 and (sys.argv[2] == 'backtest' or sys.argv[2] == 'both'):
             notebook_1_ret_val = Queue()
@@ -401,7 +574,9 @@ if __name__ == '__main__':
             notebook_3_ret_val = Queue()
             notebook_4_ret_val = Queue()
             notebook_5_ret_val = Queue()
-            notebooks_index = 0
+
+            if not num_of_pending_kmeans_tasks.count() > 0 and num_of_kmeans_tasks_running.count() > 0:
+                notebooks_index = 0
 
             print('Pending backtests: ', num_of_pending_backtests.iloc[0])
             if num_of_backtests_running.count() > 0:
@@ -487,10 +662,13 @@ if __name__ == '__main__':
                     notebook_3_ret_val = Queue()
                     notebook_4_ret_val = Queue()
                     notebook_5_ret_val = Queue()
-                    notebooks_index = 0
+
+                    if not num_of_pending_kmeans_tasks.count() > 0 and num_of_kmeans_tasks_running.count() > 0:
+                        notebooks_index = 0
+                        
                     i = 0
 
-                    while i < backtests_notebooks_to_run.count():
+                    while i < num_of_pending_backtests.iloc[0]:
                         if i < num_of_pending_backtests.iloc[0] or i < 5:
                             notebook_1 = Process(target=run_notebook,
                                                  args=(backtests_notebooks_to_run[notebooks_index],
