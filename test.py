d3d00e340b62e3c9172301ba2897adf7ca437eba
Mubbashir Ali <mubbashirali35@gmail.com>
2021-05-15 20:59:37 +0500

    Update: Cookies updated

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ from actions_on_notebook import run_notebook
 from multiprocessing import Queue
 
 ret_val = Queue()
-run_notebook('gctw39G', ret_val)
+run_notebook('gctw33G', ret_val)
 print('Status returned', ret_val.get())
