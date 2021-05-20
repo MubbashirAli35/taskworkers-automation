29f84e9b0a0ce6654b6cba704b91eda87faf7383
Mubbashir Ali <mubbashirali35@gmail.com>
2021-05-20 23:13:14 +0500

    Update: Check for Backend not available updated

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ from actions_on_notebook import run_notebook
 from multiprocessing import Queue
 
 ret_val = Queue()
-run_notebook('gctw33G', ret_val)
+run_notebook('gctw21E', ret_val)
 print('Status returned', ret_val.get())
