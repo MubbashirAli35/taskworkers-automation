29f8d9bae599520541ab7c8c273ca47f84f5741f
Mubbashir Ali <mubbashirali35@gmail.com>
2021-06-08 03:30:36 +0500

    Update: Time added in created_by column when error session closed is updated

--- reschedule_failed_tasks.py
+++ reschedule_failed_tasks.py
@@ -1,17 +1,38 @@
 import psycopg2
+import datetime as dt
+
+# def update_error_session_closed(connection):
+#     query_to_use = """update task_manager_queue
+#                         set queue_state='New' , queue_status = 'Waiting For Task Manager'
+#                         where experiment_id in (
+#                         select experiment_id from task_manager_queue
+#                         where  (queue_type='BackTestTask' or queue_type = 'TrainingTask') and queue_state='Failed' and
+#                         task_start_time::date > (now()-interval '1 day')::date
+#                              and extract(epoch from (now()- task_heartbeat_time ))/60 > 15
+#                         and (queue_status = 'Error Session Closed'))"""
+#     curr = connection.cursor()
+#     curr.execute(query_to_use)
+#     curr.close()
 
 
 def update_error_session_closed(connection):
-    query_to_use = """update task_manager_queue
-                        Set queue_state='New' , queue_status = 'Waiting For Task Manager'
-                        where experiment_id in (
-                        select experiment_id from task_manager_queue
-                        where  (queue_type='BackTestTask' or queue_type = 'TrainingTask') and queue_state='Failed' and 
-                        task_start_time::date > (now()-interval '1 day')::date 
-                             and extract(epoch from (now()- task_heartbeat_time ))/60 > 15
-                        and (queue_status = 'Error Session Closed'))"""
+    query_to_use = """update task_manager_queue 
+                      set queue_state = (%s), queue_status = (%s),
+                      created_by = (%s) 
+                      where experiment_id in (
+                        select experiment_id from task_manager_queue 
+                        where (queue_type = 'BackTestTask' or queue_type = 'TrainingTask')
+                        and queue_state = 'Failed' 
+                        and task_start_time >= now() - interval '1 Day'
+                        and queue_status = 'Error Session Closed')"""
+
     curr = connection.cursor()
-    curr.execute(query_to_use)
+    curr.execute(query_to_use,
+                 ('New',
+                  'Waiting For Task Manager',
+                  'Error Session Closed reset by Colab Manager at ' + str(dt.datetime.utcnow())))
+    print(curr.rowcount)
+    connection.commit()
     curr.close()
 
 
@@ -21,9 +42,9 @@ def set_backtest_master_done_to_New(connection):
                     set queue_state = 'New' , queue_status='Waiting For Task Manager'
                     where queue_type='BackTestMaster' and queue_state = 'Done' and  parent_exp_id IN 
                     (select distinct parent_exp_id from task_manager_queue
-                    where (queue_type = 'BackTestTask') and queue_state='New'
-                    and parent_exp_id in (select parent_exp_id from task_manager_queue where
-                    queue_type='BackTestMaster' and queue_state = 'Done'))""")
+                    where queue_type = 'BackTestTask' and queue_state='New')""")
+    print(curr.rowcount)
+    connection.commit()
     curr.close()
 
 
@@ -39,6 +60,8 @@ def set_training_master_done_to_new(connection):
                     parent_exp_id from task_manager_queue where 
                     queue_type = 'TrainingTask' and
                     queue_state = 'New')""")
+    print(curr.rowcount)
+    connection.commit()
     curr.close()
 
 
@@ -53,3 +76,4 @@ if __name__ == '__main__':
 
     set_backtest_master_done_to_New(conn)
     set_training_master_done_to_new(conn)
+    conn.close()
